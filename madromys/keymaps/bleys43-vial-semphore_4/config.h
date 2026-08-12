9bf6022e748ea96b399f364331390297b95f0a67
Chris O'Callaghan <christopher.l.ocallaghan@gmail.com>
2026-08-11 20:03:36 -0400

    added entire ploopyco vial keyboard directory, along with both highres scroll and non-highres scroll keymaps, as well as the initial highres scroll uf2 compiled firmware.

---
+++ madromys/keymaps/bleys43-vial-semphore_4/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3D, 0x23, 0x68, 0xC4, 0x1F, 0x4E, 0x55, 0x8C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 5 }
+
+#define PLOOPY_DRAGSCROLL_INVERT
+#define PLOOPY_DRAGSCROLL_MOMENTARY
+#define PLOOPY_DRAGSCROLL_DIVISOR_V 4.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_H 4.0
+
